5a5730a6c42642fd591b1db1e63cfd51adc0a63a
xzg5101 <guoxiaoyang11@outlook.com>
2023-05-31 04:46:30 -0400

    use legacy light

--- main.js
+++ main.js
@@ -3,39 +3,14 @@ import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 import { Fish } from "./fish.js";
 import { Tank } from "./tank.js";
 import { createLights } from "./createLight.js";
-//set up scene and camera
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(
-  45,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
-);
-camera.position.set(200, 0, 0);
-//camera.setRotationFromAxisAngle(new THREE.Vector3(0, 1, 0), Math.PI / 4);
-camera.lookAt(0, 0, 0);
-
-const renderer = new THREE.WebGLRenderer();
-
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
-
-// setup tank
-const tank = new Tank(150, 150, 250);
-//const tank = new Tank(100, 100, 100);
-const tank_material = new THREE.LineBasicMaterial({ color: 0xffffff });
-const tank_points = tank.get_edges();
-const tank_geo = new THREE.BufferGeometry().setFromPoints(tank_points);
-const tank_line = new THREE.Line(tank_geo, tank_material);
-tank_line.visible = false;
-scene.add(tank_line);
-const fish_cnt = 10;
-const fish_size = 7;
-const light = createLights();
-
-scene.add(light);
 
 // functions
+
+function createRenderer() {
+  const renderer = new THREE.WebGLRenderer();
+  renderer.useLegacyLights = false;
+  return renderer;
+}
 
 function add_fish(cnt) {
   let fish_list = [];
@@ -59,7 +34,6 @@ function animate() {
     fish_list[i].update_animation();
     fish_list[i].update_position(tank.get_dimensions(), fish_list, mouse);
   }
-  //console.log("ani", mouse.x, mouse.y);
   renderer.render(scene, camera);
 }
 
@@ -70,6 +44,36 @@ function onMouseMove(event) {
   mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
   //console.log(mouse.x, mouse.y);
 }
+
+//set up scene and camera
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  45,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+camera.position.set(200, 0, 0);
+camera.lookAt(0, 0, 0);
+
+const renderer = createRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+// setup tank
+const tank = new Tank(150, 150, 250);
+//const tank = new Tank(100, 100, 100);
+const tank_material = new THREE.LineBasicMaterial({ color: 0xffffff });
+const tank_points = tank.get_edges();
+const tank_geo = new THREE.BufferGeometry().setFromPoints(tank_points);
+const tank_line = new THREE.Line(tank_geo, tank_material);
+tank_line.visible = false;
+scene.add(tank_line);
+const fish_cnt = 10;
+const fish_size = 7;
+const light = createLights();
+
+scene.add(light);
 
 let fish_list = add_fish(fish_cnt);
 //fish_list[0].set_rotation_z(Math.PI / 2);
