e0e9c1dfdd49ec22f3c356676b00e03e2a375a44
xzg5101 <guoxiaoyang11@outlook.com>
2023-05-31 02:48:30 -0400

    Revert "Initial Commit"
    This reverts commit 72112f25e779be6f9c2c5ee7dd73a3bdb0040f43.

--- utils.js
+++
@@ -1,8 +0,0 @@
-import * as THREE from "three";
-export function sleep(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
-
-export function random_color() {
-  return new THREE.Color(Math.random(), Math.random(), Math.random());
-}
